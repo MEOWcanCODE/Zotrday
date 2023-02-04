4920278a98d76243910c5937ae8f37c5d4a84528
Eric Chau <chaueq@uci.edu>
2023-02-04 13:54:02 -0800

    Interpret website text as json object

--- backend/scraper.py
+++ backend/scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+import json
 
 options = Options()
 options.headless = True
@@ -15,6 +16,8 @@ element = WebDriverWait(browser, 60).until(EC.presence_of_all_elements_located((
 
 names = browser.find_elements(By.CSS_SELECTOR, 'li.study ')
 
-
-
-
+for items in names:
+    attributes = items.text.split('\n')
+    str = (json.dumps({'Name': attributes[1], 'Location': attributes[2], 'Time':attributes[3]}))
+    print(str)
+    print()
