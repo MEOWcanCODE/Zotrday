abbe9ff3e490094974829f59b1e359d951ba19cd
Scott Pham <scotttp1@uci.edu>
2023-02-04 13:44:29 -0800

    added headless option

--- backend/scraper.py
+++ backend/scraper.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+options = Options()
+options.headless = True
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 browser.get('https://www.lib.uci.edu/study-space-locator')
 
 element = WebDriverWait(browser, 60).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li.study ')))
 
 names = browser.find_elements(By.CSS_SELECTOR, 'li.study ')
-for items in names:
-    print(items.text,'\n')
+
 
 
 
