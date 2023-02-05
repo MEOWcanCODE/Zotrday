1636ca4d3b31a8f7e854a2e6a973003738692345
Eric Chau <chaueq@uci.edu>
2023-02-04 18:37:30 -0800

    scrape() now returns json formatted string

--- backend/scraper.py
+++ backend/scraper.py
@@ -7,48 +7,50 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import json
 
+def scrape():
+    options = Options()
+    options.add_argument('--headless')
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    browser.get('https://www.lib.uci.edu/study-space-locator')
 
-options = Options()
-options.add_argument('--headless')
-browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-browser.get('https://www.lib.uci.edu/study-space-locator')
+    element = WebDriverWait(browser, 60).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li.study ')))
 
-element = WebDriverWait(browser, 60).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li.study ')))
-
-click_button = browser.find_element(By.ID, "f_submit")
-click_button.click()
-names = browser.find_elements(By.CSS_SELECTOR, 'li.study ')
+    click_button = browser.find_element(By.ID, "f_submit")
+    click_button.click()
+    names = browser.find_elements(By.CSS_SELECTOR, 'li.study ')
 
 
-study_list = {}
-for items in names:
-    id_num = items.get_attribute("id")
-    attributes = items.text.split('\n')
-    if attributes[3] == "today:":
-        attributes[3] = "Hours not listed"
-    study_list[id_num] = {'Name': attributes[1], 'Location': attributes[2], 'Time':attributes[3], 'Image-URL':"", 'Amenities':[]}
+    study_list = {}
+    for items in names:
+        id_num = items.get_attribute("id")
+        attributes = items.text.split('\n')
+        if attributes[3] == "today:":
+            attributes[3] = "Hours not listed"
+        study_list[id_num] = {'Name': attributes[1], 'Location': attributes[2], 'Time':attributes[3], 'Image-URL':"", 'Amenities':[]}
 
-src = browser.find_elements(By.XPATH,"//img[contains(@class,'center-image image-responsive')]")
-img_list = []
-for items in src:
-    img_list.append(items.get_attribute("src"))
+    src = browser.find_elements(By.XPATH,"//img[contains(@class,'center-image image-responsive')]")
+    img_list = []
+    for items in src:
+        img_list.append(items.get_attribute("src"))
 
-i = 0
-for spot in study_list:
-    study_list[spot]['Image-URL'] = img_list[i]
-    i += 1
+    i = 0
+    for spot in study_list:
+        study_list[spot]['Image-URL'] = img_list[i]
+        i += 1
 
-amenities_lst = browser.find_elements(By.CLASS_NAME, "amenities-icon")
-for icons in amenities_lst:
-    amenity = icons.get_attribute('class')[21:]
+    amenities_lst = browser.find_elements(By.CLASS_NAME, "amenities-icon")
+    for icons in amenities_lst:
+        amenity = icons.get_attribute('class')[21:]
 
-    div_class = icons.find_element(By.XPATH, "..")
-    parent = div_class.find_element(By.XPATH, "..")
+        div_class = icons.find_element(By.XPATH, "..")
+        parent = div_class.find_element(By.XPATH, "..")
 
-    parent_id = parent.get_attribute("id")
-    study_list[parent_id]['Amenities'].append(amenity)
+        parent_id = parent.get_attribute("id")
+        study_list[parent_id]['Amenities'].append(amenity)
 
-for s in study_list:
-    print(json.dumps(study_list[s]))
-    print()
+    json_object = json.loads(json.dumps(study_list))
+    json_formatted_str = json.dumps(json_object, indent=2)
+    return json_formatted_str
 
+if __name__ == '__main__':
+    print(scrape())
