7580b17dfe03484e45091472741f38d058e5e302
Scott Pham <scotttp1@uci.edu>
2023-02-04 19:19:07 -0800

    testing scraper

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from scraper import scraper
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-    return jsonify({'message': 'Hey, everything works!!'})
+    return jsonify(scraper())
