36807bee23a0b722c858589ea4d7d90042d9d52c
Eric Chau <chaueq@uci.edu>
2023-02-04 17:22:31 -0800

    Added image URLs to json objects

--- backend/scraper.py
+++ backend/scraper.py
@@ -19,13 +19,23 @@ click_button = browser.find_element(By.ID, "f_submit")
 click_button.click()
 names = browser.find_elements(By.CSS_SELECTOR, 'li.study ')
 
-src = browser.find_elements(By.XPATH,"//img[contains(@class,'center-image image-responsive')]")
-for items in src:
-    print(items.get_attribute("src"))
 
-
+study_list = {}
 for items in names:
+    id_num = items.get_attribute("id")
     attributes = items.text.split('\n')
-    str = (json.dumps({'Name': attributes[1], 'Location': attributes[2], 'Time':attributes[3]}))
+    if attributes[3] == "today:":
+        attributes[3] = "Hours not listed"
+    study_list[id_num] = {'Name': attributes[1], 'Location': attributes[2], 'Time':attributes[3], 'Image-URL':"", 'Amenities':[]}
+
+src = browser.find_elements(By.XPATH,"//img[contains(@class,'center-image image-responsive')]")
+img_list = []
+for items in src:
+    img_list.append(items.get_attribute("src"))
+i = 0
+for spot in study_list:
+    study_list[spot]['Image-URL'] = img_list[i]
+    str = json.dumps(study_list[spot])
     print(str)
     print()
+    i += 1
