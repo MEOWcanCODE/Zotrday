5151d8f33e7d6639e60008c5c23ad53d38ae0d28
Scott Pham <scotttp1@uci.edu>
2023-02-04 19:23:04 -0800

    changed scraper

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from scraper import scraper
+from scraper import scrape
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-    return jsonify(scraper())
+    return jsonify(scrape())
